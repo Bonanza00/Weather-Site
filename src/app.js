5a03d6f1462b5f27163d453f817ae40cc7a85c10
FatsoFrog <vilgot.gudmundsson@gmail.com>
2020-01-04 22:49:33 -0800

    cleaned up code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+
 const app = express()
 const port = process.env.PORT || 3000
 
