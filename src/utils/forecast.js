48d76e04c7928acfb6aa829e399354522a596859
FatsoFrog <vilgot.gudmundsson@gmail.com>
2020-01-05 16:26:00 -0800

    Fixed spelling errors

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Location not found', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The High for today is ' + body.daily.data[0].temperatureHigh + '. Whlie the Low for today is ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The High for today is ' + body.daily.data[0].temperatureHigh + '. While the Low for today is ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain')
         }                
     })
 }
